693b4a1482d90a0ac8f940f29428936ab0a8076a
Zoe Meng <zoebo1225@gmail.com>
2021-04-19 16:33:58 -0700

    added like and unlike function

--- pages/Playlist.js
+++ pages/Playlist.js
@@ -1,11 +1,10 @@
 import React,{useEffect, useState} from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Header, Text, ListItem, Avatar, Image, Icon } from 'react-native-elements';
+import { StyleSheet, View, Alert} from 'react-native';
+import { Header, Text, ListItem, Avatar, Image } from 'react-native-elements';
 import Axios from 'axios';
 import {BASE_URL, API_KEY} from '../constance';
 
-import { AntDesign } from '@expo/vector-icons'; 
-
+import { Feather } from '@expo/vector-icons'; 
 
 const Playlist = () => {
   const [playnow, setPlaynow] = useState([])
@@ -30,25 +29,33 @@ const Playlist = () => {
     })
   } 
 
-
   useEffect(() => {
     getData()
     const interval = setInterval(() => {getData()}, 30000)
     return () => clearInterval(interval)
   }, [])
 
-  
 
-
-  const likehandler = id => {
-    Axios.get(url, {
+  const likehandler = (id) => {
+    const VOTEUP_URL = `${BASE_URL}/vote_up?&pick_id=${id}`;
+     Axios.post(VOTEUP_URL, {pick_id: id}, {
       headers: {
-        authorization: API_KEY
+        Authorization: API_KEY,
       }
-    }).then(res => {
-      return res.data.response.queue.find(q => q?.pick_id === id)
-    }).catch(e => console.log(e))
+    }).then(res => console.log(res))
+      .catch(e => console.log(e.message))
   }
+
+  const unlikehandler = id => {
+    const VOTEDOWN_URL = `${BASE_URL}/vote_down?&pick_id=${id}`;
+    Axios.post(VOTEDOWN_URL, {pick_id: id}, {
+      headers: {
+        Authorization: API_KEY,
+      }
+    }).then(res => console.log(res))
+      .catch(e => console.log(e.message))
+  } 
+
 
 
   
@@ -82,8 +89,11 @@ const Playlist = () => {
                 <ListItem.Title>{item.artist}</ListItem.Title>
                 <ListItem.Subtitle>{item.song}</ListItem.Subtitle>    
               </ListItem.Content>
-              <AntDesign onClick = {() => {likehandler(item.pick_id)}} name="like2" size={20} color="black" /> 
+              <Feather onClick = {() => {likehandler(item.pick_id)}} name="thumbs-up" size={20} color="black" /> 
               <Text>{item.likes}</Text>
+
+              <Feather name="thumbs-down" size={20} color="black" onClick = {() => {unlikehandler(item.pick_id)}} />
+              <Text>{item.dislikes}</Text>
             </ListItem>
           ))
       }
